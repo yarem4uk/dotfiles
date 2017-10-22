6a3ba05ef5a57771b86b09537f5e0a2620dda3e3
alex <yarem4uk@gmail.com>
2017-10-23 00:59:04 +0300

    add new folder scripts and rename files without dot

--- scripts/mklinks.sh
+++ scripts/mklinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-dir=~/dotfiles
-files=".zshrc .aliases .vimrc .tmux.conf"
+dir=~/dotfiles/scripts
+files="zshrc aliases vimrc tmux.conf"
 
 cd $dir
 
@@ -9,5 +9,5 @@ echo '...done'
 for file in $files; 
 do 
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+    ln -s $dir/$file ~/.$file
 done
